5d165a337f210186d7cb1fb788cdc035355bec9a
markup54 <marzenakupka@tlen.pl>
2024-09-12 10:37:55 +0200

    wypelnianie tablicy wartociami losowymi i wypisywanie jej na ekranie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        //tablica
+        /*
+        tablica musi mieć zdefiniowany rozmiar,
+        którego potem nie można zmienić
+        tablica przechowuje typy proste i złożone
+         */
+        int [] tablicaLiczbLosowych = new int[6];
+        //losujemy z zakresu 1 do 100
+        for (int i = 0; i < tablicaLiczbLosowych.length ; i++) {
+            tablicaLiczbLosowych[i] = (int)(Math.random()*100);
+        }
+        //wypisywanie tablicy na ekranie
+        for (int element:tablicaLiczbLosowych) {
+            System.out.print(element+", ");
+        }
+
+
     }
 }
