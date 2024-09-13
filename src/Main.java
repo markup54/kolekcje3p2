c753068964461e17a1d295dbcfcb205f216c1291
markup54 <marzenakupka@tlen.pl>
2024-09-13 11:39:07 +0200

    sito

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,39 @@ public class Main {
     public static void main(String[] args) {
        Gra gra1 = new Gra();
        gra1.zagraj();
-
+        System.out.println("pierwsze"+sitoEratostenesa(200));
     }
 
+    //do kartkowki
+    static ArrayList<Integer> sitoEratostenesa(int liczba){
+        //przygotowanie listy z wartosciami logicznymi
+        ArrayList<Boolean> logiczne = new ArrayList<>();
+        logiczne.add(false);
+        logiczne.add(false);
+        for (int i = 2; i <liczba ; i++) {
+            logiczne.add(true);
+        }
+        //wykrelsanie wielokrotnosci liczb pierwszych
+        for (int i = 0; i < liczba; i++) {
+            if(logiczne.get(i).equals(true)){
+                //wykreslanie zmiana na false
+                for(int k = i*2 ; k<liczba ; k = k +i)
+                {
+                    logiczne.set(k,false);
+                }
+            }
+        }
+        ArrayList<Integer> pierwsze = new ArrayList<>();
+        for (int i = 0; i < logiczne.size(); i++) {
+            if(logiczne.get(i).equals(true)) {
+                pierwsze.add(i);
+            }
+        }
+        return pierwsze;
+
+
+
+    }
 
 
 }
